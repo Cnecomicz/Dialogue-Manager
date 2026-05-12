f8629f0bf256d5a1c7efa5a88504a832d752a80b
Eric <eric@locallyringed.space>
2026-05-12 18:03:43 -0500

    Green: test_creation_of_vertices_and_edges

--- src/dialogue_manager/graph_builder.py
+++ src/dialogue_manager/graph_builder.py
@@ -1,2 +1,9 @@
+from yaml import safe_load
+
 class GraphBuilder:
-    pass
+    def __init__(self, yaml_file: str):
+        with open(yaml_file, 'r') as f:
+            yaml_data = safe_load(f)
+        self.name = yaml_data["name"]
+        self.vertices = yaml_data["vertices"]
+        self.edges = yaml_data["edges"]
