8f51d3b1fe5eda67b5ea2dee65eb9e44eda83022
Eric <eric@locallyringed.space>
2026-05-13 16:53:10 -0500

    Green: test_starting_current_vertex

--- src/dialogue_manager/graph_manager.py
+++ src/dialogue_manager/graph_manager.py
@@ -1,2 +1,7 @@
+from dialogue_manager.graph import Graph
+
 class GraphManager:
-    pass
+    def __init__(self, graph: Graph, game_state: "GameState"):
+        self.graph = graph
+        self.game_state = game_state
+        self.current_vertex = self.graph.vertex_dict["begin"]
