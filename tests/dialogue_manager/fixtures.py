25f74929521dbb622209fb3e520928c195b92e44
Eric <eric@locallyringed.space>
2026-05-13 16:59:10 -0500

    Red: test_get_valid_edges

--- tests/dialogue_manager/fixtures.py
+++ tests/dialogue_manager/fixtures.py
@@ -7,5 +7,5 @@ def hello_world_graph():
     return Graph("data/hello_world.yaml")
 
 @fixture
-def mock_game_state():
+def mock_game_state_with_gold():
     return
--- tests/dialogue_manager/test_graph_manager.py
+++ tests/dialogue_manager/test_graph_manager.py
@@ -1,9 +1,17 @@
-from fixtures import hello_world_graph, mock_game_state
+from fixtures import hello_world_graph, mock_game_state_with_gold
 
 from dialogue_manager.graph_manager import GraphManager
 
 # GraphManager has a current vertex that starts with "begin"
 def test_starting_current_vertex(hello_world_graph):
-    graph_manager = GraphManager(hello_world_graph, mock_game_state)
+    graph_manager = GraphManager(hello_world_graph, mock_game_state_with_gold)
     assert graph_manager.current_vertex == hello_world_graph.vertex_dict["begin"]
 
+# GraphManager can return valid edge choices for your current vertex
+def test_get_valid_edges(hello_world_graph):
+    graph_manager = GraphManager(hello_world_graph, mock_game_state_with_gold)
+    assert len(graph_manager.current_edges) == 2
+    assert graph_manager.current_edges == {
+        "begin_to_buy_flower": hello_world_graph.edge_dict["begin_to_buy_flower"],
+        "begin_to_end": hello_world_graph.edge_dict["begin_to_end"]
+    }
