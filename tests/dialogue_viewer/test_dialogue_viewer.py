a80f76a06ef3a0ba8b25f699d765620b1157cc7c
Eric <eric@locallyringed.space>
2026-05-16 21:49:42 -0500

    Red: test_dialogue_viewer_viewing

--- tests/dialogue_viewer/test_dialogue_viewer.py
+++ tests/dialogue_viewer/test_dialogue_viewer.py
@@ -5,3 +5,8 @@ from dialogue_viewer.dialogue_viewer import DialogueViewer
 # The DialogueViewer takes in a Graph
 def test_dialogue_viewer_creation(hello_world_graph):
     dialogue_viewer = DialogueViewer(hello_world_graph)
+
+# The DialogueViewer spits out an image file
+def test_dialogue_viewer_viewing(hello_world_graph):
+    dialogue_viewer = DialogueViewer(hello_world_graph)
+    dialogue_viewer.render()
