a64733c25f7a36a693de68eb095002b9e7e01044
Eric <eric@locallyringed.space>
2026-05-17 11:08:59 -0500

    Red: test_edge_filter_1_and_filter_2

--- tests/dialogue_manager/fixtures.py
+++ tests/dialogue_manager/fixtures.py
@@ -7,7 +7,7 @@ def hello_world_graph():
     return Graph("data/hello_world.yaml")
 
 @fixture
-def mock_game_state_with_gold():
+def mock_game_state():
     class MockPlayer:
         def __init__(self):
             self.gold = 2
@@ -15,6 +15,22 @@ def mock_game_state_with_gold():
     class MockAlice:
         def __init__(self):
             self.inventory = ["Flower", "Flower"]
+    class MockGameState:
+        def __init__(self):
+            self.player = MockPlayer()
+            self.alice = MockAlice()
+    game_state = MockGameState()
+    return game_state
+
+@fixture
+def mock_game_state_with_limited_inventory():
+    class MockPlayer:
+        def __init__(self):
+            self.gold = 2
+            self.inventory = []
+    class MockAlice:
+        def __init__(self):
+            self.inventory = ["Flower"]
     class MockGameState:
         def __init__(self):
             self.player = MockPlayer()
--- tests/dialogue_manager/test_graph_manager.py
+++ tests/dialogue_manager/test_graph_manager.py
@@ -1,17 +1,19 @@
 from pytest import raises
 
-from fixtures import hello_world_graph, mock_game_state_with_gold
+from fixtures import (
+    hello_world_graph, mock_game_state, mock_game_state_with_limited_inventory
+)
 
 from dialogue_manager.graph_manager import GraphManager, InvalidEdgeError
 
 # GraphManager has a current vertex that starts with "begin"
-def test_starting_current_vertex(hello_world_graph):
-    graph_manager = GraphManager(hello_world_graph, mock_game_state_with_gold)
+def test_starting_current_vertex(hello_world_graph, mock_game_state):
+    graph_manager = GraphManager(hello_world_graph, mock_game_state)
     assert graph_manager.current_vertex == hello_world_graph.vertex_dict["begin"]
 
 # GraphManager keeps track of valid edge choices for your current vertex
-def test_has_valid_edges(hello_world_graph, mock_game_state_with_gold):
-    graph_manager = GraphManager(hello_world_graph, mock_game_state_with_gold)
+def test_has_valid_edges(hello_world_graph, mock_game_state):
+    graph_manager = GraphManager(hello_world_graph, mock_game_state)
     assert len(graph_manager.current_edges) == 2
     assert graph_manager.current_edges == {
         "begin_to_buy_flower": hello_world_graph.edge_dict["begin_to_buy_flower"],
@@ -19,41 +21,49 @@ def test_has_valid_edges(hello_world_graph, mock_game_state_with_gold):
     }
 
 # You can't select an edge if it's not valid
-def test_error_when_selecting_invalid_edge(hello_world_graph, mock_game_state_with_gold):
-    graph_manager = GraphManager(hello_world_graph, mock_game_state_with_gold)
+def test_error_when_selecting_invalid_edge(hello_world_graph, mock_game_state):
+    graph_manager = GraphManager(hello_world_graph, mock_game_state)
     with raises(InvalidEdgeError):
         graph_manager.select("begin_to_insufficient_gold")
 
 # Selecting a valid edge causes its effects to occur
-def test_select_edge_to_cause_effects(hello_world_graph, mock_game_state_with_gold):
-    graph_manager = GraphManager(hello_world_graph, mock_game_state_with_gold)
+def test_select_edge_to_cause_effects(hello_world_graph, mock_game_state):
+    graph_manager = GraphManager(hello_world_graph, mock_game_state)
     graph_manager.select("begin_to_buy_flower")
-    assert mock_game_state_with_gold.player.gold == 1
+    assert mock_game_state.player.gold == 1
 
 # Selecting an edge updates your current vertex
-def test_select_edge_to_go_to_next_vertex(hello_world_graph, mock_game_state_with_gold):
-    graph_manager = GraphManager(hello_world_graph, mock_game_state_with_gold)
+def test_select_edge_to_go_to_next_vertex(hello_world_graph, mock_game_state):
+    graph_manager = GraphManager(hello_world_graph, mock_game_state)
     graph_manager.select("begin_to_buy_flower")
     assert graph_manager.current_vertex == hello_world_graph.vertex_dict["buy_flower"]
 
 # If the game state changes mid conversation, the valid edges change to reflect that
-def test_error_when_selecting_invalid_edge_that_was_previously_valid(hello_world_graph, mock_game_state_with_gold):
-    graph_manager = GraphManager(hello_world_graph, mock_game_state_with_gold)
+def test_error_when_selecting_invalid_edge_that_was_previously_valid(hello_world_graph, mock_game_state):
+    graph_manager = GraphManager(hello_world_graph, mock_game_state)
     graph_manager.select("begin_to_buy_flower")
     graph_manager.select("buy_flower_to_buy_flower")
     with raises(InvalidEdgeError):
         graph_manager.select("buy_flower_to_buy_flower")
 
 # You can get a list of valid edges as strs
-def test_get_valid_edges(hello_world_graph, mock_game_state_with_gold):
-    graph_manager = GraphManager(hello_world_graph, mock_game_state_with_gold)
+def test_get_valid_edges(hello_world_graph, mock_game_state):
+    graph_manager = GraphManager(hello_world_graph, mock_game_state)
     assert graph_manager.get_current_edges() == ["begin_to_buy_flower", "begin_to_end"]
 
 # Entering a vertex causes its effects to procure
-def test_new_vertex_causes_effects(hello_world_graph, mock_game_state_with_gold):
-    graph_manager = GraphManager(hello_world_graph, mock_game_state_with_gold)
-    assert mock_game_state_with_gold.player.inventory == []
-    assert mock_game_state_with_gold.alice.inventory == ["Flower", "Flower"]
+def test_new_vertex_causes_effects(hello_world_graph, mock_game_state):
+    graph_manager = GraphManager(hello_world_graph, mock_game_state)
+    assert mock_game_state.player.inventory == []
+    assert mock_game_state.alice.inventory == ["Flower", "Flower"]
     graph_manager.select("begin_to_buy_flower")
-    assert mock_game_state_with_gold.player.inventory == ["Flower"]
-    assert mock_game_state_with_gold.alice.inventory == ["Flower"]
+    assert mock_game_state.player.inventory == ["Flower"]
+    assert mock_game_state.alice.inventory == ["Flower"]
+
+# Edge filters can be ANDed
+def test_edge_filter_1_and_filter_2(hello_world_graph, mock_game_state_with_limited_inventory):
+    graph_manager = GraphManager(hello_world_graph, mock_game_state_with_limited_inventory)
+    graph_manager.select("begin_to_buy_flower")
+    with raises(InvalidEdgeError):
+        graph_manager.select("buy_flower_to_buy_flower")
+    graph_manager.select("buy_flower_to_insufficient_flowers")
