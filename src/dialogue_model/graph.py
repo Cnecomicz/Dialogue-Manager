e9bfe872104daf5108930e8195d15bbe02b4f3d3
Eric <eric@locallyringed.space>
2026-05-19 15:56:11 -0500

    Refactor: removing unnecessary Graph methods

--- src/dialogue_model/graph.py
+++ src/dialogue_model/graph.py
@@ -11,17 +11,11 @@ class Graph:
             with open(yaml_file, 'r') as f:
                 yaml_data = safe_load(f)
         self.name = yaml_data["name"]
-        self.vertex_dict = self.initial_populate_vertices(yaml_data)
-        self.edge_dict = self.initial_populate_edges(yaml_data)
-
-    def initial_populate_edges(self, yaml_data: dict) -> dict[str, Edge]:
-        return {
-            edge_name: Edge(edge_name, data) 
-            for edge_name, data in yaml_data["edges"].items()
-        }
-
-    def initial_populate_vertices(self, yaml_data: dict) -> dict[str, Vertex]:
-        return {
-            vertex_name: Vertex(vertex_name, data) 
+        self.vertex_dict = {
+            vertex_name: Vertex(vertex_name, data)
             for vertex_name, data in yaml_data["vertices"].items()
+        }
+        self.edge_dict = {
+            edge_name: Edge(edge_name, data)
+            for edge_name, data in yaml_data["edges"].items()
         }
