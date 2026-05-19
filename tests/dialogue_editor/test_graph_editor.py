a3c5024995badc9816ed7285fd4d5dbe86f2aecf
Eric <eric@locallyringed.space>
2026-05-19 17:13:25 -0500

    Red: test_editing_edge

--- tests/dialogue_editor/test_graph_editor.py
+++ tests/dialogue_editor/test_graph_editor.py
@@ -56,6 +56,22 @@ def test_add_new_edge_without_to_vertex():
     assert len(graph_editor.graph.vertex_dict) == 2
 
 # You can edit existing edges
+def test_editing_edge():
+    graph_editor = GraphEditor()
+    graph_editor.add_vertex("Hello world.")
+    graph_editor.add_vertex("Goodbye world.")
+    graph_editor.add_edge("vertex_0", "vertex_1", "This is an edge.", [{"type": "check_value", "path": "player.gold", "op": ">=", "value": 1}], [{"type": "modify_value", "target": "player.gold", "delta": -1}])
+    graph_editor.edit_from_vertex("edge_0", "vertex_1")
+    graph_editor.edit_to_vertex("edge_0", "vertex_0")
+    graph_editor.edit_edge_text("edge_0", "Updated text.")
+    graph_editor.remove_predicate("edge_0", "player.gold >= 1")
+    graph_editor.add_predicate("edge_0", "Bomb not in player.inventory")
+    graph_editor.remove_effect("edge_0", "player.gold = player.gold-1")
+    graph_editor.add_effect("edge_0", "player.inventory.append(Bomb)")
+    assert graph_editor.graph.edge_dict["edge_0"] == Edge(
+        "edge_0", {"from_vertex": "vertex_1", "to_vertex": "vertex_0", "text": "Updated text.", "predicates": [{"type": "check_list", "path": "player.inventory", "op": "not in", "value": "Bomb"},], "effects": [{"type": "modify_list", "target": "player.inventory", "method": "append", "value": "Bomb"}]}
+    )
+
 
 # You can remove an edge
 
