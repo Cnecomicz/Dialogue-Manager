9b68d8eda39e31e51e942013c02be73affb8f268
Eric <eric@locallyringed.space>
2026-05-20 17:52:59 -0500

    Green: test_cytoscape_edges

--- src/dialogue_viewer/cytoscape_adapter.py
+++ src/dialogue_viewer/cytoscape_adapter.py
@@ -8,8 +8,21 @@ class CytoscapeAdapter:
         self.graph = graph
 
     @property
+    def edges(self) -> list[dict[str, dict[str, str]]]:
+        edges = []
+        for edge_name, edge in self.graph.edge_dict.items():
+            edges.append(
+                {"data": {"source": edge.from_vertex, "target": edge_name}}
+            )
+            edges.append(
+                {"data": {"source": edge_name, "target": edge.to_vertex}}
+            )
+        return edges
+
+    @property
     def nodes(self) -> list[dict[str, dict[str, str]]]:
         nodes = []
+        # Nodes from vertices
         for vertex_name, vertex in self.graph.vertex_dict.items():
             effects_text = self.get_effects_text(vertex.effects)
             dialogue_text = f"TEXT:\n{vertex.text}"
@@ -18,7 +31,17 @@ class CytoscapeAdapter:
             else:
                 text = dialogue_text
             nodes.append({"data": {"id": vertex_name, "label": text}})
-        print(nodes)
+        # Nodes from edges
+        for edge_name, edge in self.graph.edge_dict.items():
+            dialogue_text = f"TEXT:\n{edge.text}\n"
+            predicates_text = self.get_predicates_text(edge.predicates)
+            effects_text = self.get_effects_text(edge.effects)
+            text = dialogue_text
+            if predicates_text:
+                text += f"\n{predicates_text}"
+            if effects_text:
+                text += f"\n{effects_text}"
+            nodes.append({"data": {"id": edge_name, "label": text}})
         return nodes
 
     def get_effects_text(self, effects: list[dict[str, str | int]]) -> str:
@@ -29,3 +52,12 @@ class CytoscapeAdapter:
         else:
             effects_text = ""
         return effects_text
+
+    def get_predicates_text(self, predicates: list[dict[str, str | int]]) -> str:
+        if predicates:
+            predicates_text = "PREDICATES:\n"
+            for predicate in predicates:
+                predicates_text += convert_predicate_to_text(predicate)+"\n"
+        else:
+            predicates_text = ""
+        return predicates_text
--- tests/dialogue_viewer/test_cytoscape_adapter.py
+++ tests/dialogue_viewer/test_cytoscape_adapter.py
@@ -7,12 +7,12 @@ def test_instantiating_adapter(hello_world_graph):
 # The adapter turns Graph.vertex_dict into Cytoscape nodes
 def test_cytoscape_nodes(hello_world_graph):
     cytoscape_adapter = CytoscapeAdapter(hello_world_graph)
-    assert len(cytoscape_adapter.nodes) == len(hello_world_graph.vertex_dict)
     assert {"data": {"id": "buy_flower", "label": "EFFECTS:\nplayer.inventory.append(Flower)\nalice.inventory.remove(Flower)\n\nTEXT:\nThanks. You now have {player.gold} gold. Want to buy another?"}} in cytoscape_adapter.nodes
 
 # The adapter turns Graph.edge_dict into Cytoscape nodes with Cytoscape edges between them
 def test_cytoscape_edges(hello_world_graph):
     cytoscape_adapter = CytoscapeAdapter(hello_world_graph)
+    assert len(cytoscape_adapter.nodes) == len(hello_world_graph.vertex_dict) + len(hello_world_graph.edge_dict)
     assert len(cytoscape_adapter.edges) == 2*len(hello_world_graph.edge_dict)
     assert {"data": {"source": "vertex_0", "target": "vertex_0_to_buy_flower"}}, {"data": {"source": "vertex_0_to_buy_flower", "target": "buy_flower"}} in cytoscape_adapter.edges
 
