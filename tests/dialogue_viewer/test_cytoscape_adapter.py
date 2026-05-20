778faa30f0f88a8aab0f8067e6ab62f297ff7de4
Eric <eric@locallyringed.space>
2026-05-20 17:32:56 -0500

    Red: test_cytoscape_nodes

--- tests/dialogue_viewer/test_cytoscape_adapter.py
+++ tests/dialogue_viewer/test_cytoscape_adapter.py
@@ -5,6 +5,10 @@ def test_instantiating_adapter(hello_world_graph):
     cytoscape_adapter = CytoscapeAdapter(hello_world_graph)
 
 # The adapter turns Graph.vertex_dict into Cytoscape nodes
+def test_cytoscape_nodes(hello_world_graph):
+    cytoscape_adapter = CytoscapeAdapter(hello_world_graph)
+    assert len(cytoscape_adapter.nodes) == len(hello_world_graph.vertex_dict)
+    assert {"data": {"id": "buy_flower", "label": "EFFECTS:\nplayer.inventory.append(Flower)\nalice.inventory.remove(Flower)\n\nTEXT:\nThanks. You now have {player.gold} gold. Want to buy another?"}} in cytoscape_adapter.nodes
 
 # The adapter turns Graph.edge_dict into Cytoscape nodes with Cytoscape edges between them
 
