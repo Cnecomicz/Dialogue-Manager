e5cda48ed9fdf009ed85aba0303ace81bbc247ec
Eric <eric@locallyringed.space>
2026-05-20 17:22:50 -0500

    Red: test_instantiating_adapter

---
+++ src/dialogue_viewer/cytoscape_adapter.py
@@ -0,0 +1,2 @@
+class CytoscapeAdapter:
+    pass
---
+++ tests/dialogue_viewer/test_cytoscape_adapter.py
@@ -0,0 +1,12 @@
+from dialogue_viewer.cytoscape_adapter import CytoscapeAdapter
+
+# The adapter takes as input a Graph
+def test_instantiating_adapter():
+    cytoscape_adapter = CytoscapeAdapter(hello_world_graph)
+
+# The adapter turns Graph.vertex_dict into Cytoscape nodes
+
+# The adapter turns Graph.edge_dict into Cytoscape nodes with Cytoscape edges between them
+
+# The Cytoscape nodes have all the text and data from the Graph vertices/edges
+
