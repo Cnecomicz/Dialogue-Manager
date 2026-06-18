acd264b1b83494be0c63548eb16a1468442c08ff
Eric <eric@locallyringed.space>
2026-06-17 21:07:29 -0500

    Red: test_get_current_vertex_text, test_get_current_edge_texts, test_get_current_turn

--- tests/dialogue_navigator/test_graph_navigator.py
+++ tests/dialogue_navigator/test_graph_navigator.py
@@ -63,3 +63,19 @@ def test_edge_predicate_1_and_predicate_2(alice_graph, mock_game_state_with_limi
     with raises(InvalidEdgeError):
         graph_navigator.select("buy_flower_to_buy_flower")
     graph_navigator.select("edge_5")
+
+# You can get the current NPC dialogue text directly
+def test_get_current_vertex_text(alice_graph, mock_game_state):
+    graph_navigator = GraphNavigator(alice_graph, mock_game_state)
+    assert graph_navigator.get_current_vertex_text() == "Hello. You have {player.gold} gold. Want to buy a flower?"
+
+# You can get current edge texts as {edge_id: edge.text}
+def test_get_current_edge_texts(alice_graph, mock_game_state):
+    graph_navigator = GraphNavigator(alice_graph, mock_game_state)
+    assert graph_navigator.get_current_edge_texts() == {"edge_0": "Yes, I'll buy a flower.", "edge_3": "No thanks."}
+
+# You can get all current turn text in one payload
+def test_get_current_turn(alice_graph, mock_game_state):
+    graph_navigator = GraphNavigator(alice_graph, mock_game_state)
+    assert graph_navigator.get_current_turn() == {"vertex_text": "Hello. You have {player.gold} gold. Want to buy a flower?", "edge_texts": {"edge_0": "Yes, I'll buy a flower.", "edge_3": "No thanks."}}
+
